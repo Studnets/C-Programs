eddb6c14d0208f4bda3dafc6212438eebaa2215f
Humpo <acomerford@albany.edu>
2014-06-25 21:32:13 -0400

    input checker

--- calculator.c
+++ calculator.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 main() {
   //functions here
+  int mark=1;
+  char input[count];
+  
   printf("Welcome to K&A 4-function calculator!");
+  
+  while (mark > 0) {
+    scanf("Enter a number: %d", &input[count]);
+    fgets(input, sizeof(input), stdin);  //Input, memory size, reads input
+    for (count = 0; count < strlen(input); ++count)
+    {
+      if (!isdigit(input[count]))
+      {
+          mark = 0;
+          break;
+      }
+      else
+        printf("Invalid input!");
+    }
+  }
+  
   return 0;
 }
